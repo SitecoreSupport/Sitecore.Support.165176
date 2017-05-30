fe47309b04f22bc19c63df225d160d73e3e25cca
Ivan Sheyenko <ivs@sitecore.net>
2017-05-30 18:04:52 +0300

    created

---
+++ src/Sitecore.Support.165176/ContactListQueryProvider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using Sitecore.ContentSearch;
+using Sitecore.Data;
+using Sitecore.ListManagement.ContentSearch.Model;
+
+namespace Sitecore.Support.ListManagement.ContentSearch
+{
+    public class ContactListQueryProvider : Sitecore.ListManagement.ContentSearch.ContactListQueryProvider
+    {
+        bool IncludeSubFolders;
+        public ContactListQueryProvider(Sitecore.ContentSearch.ISearchIndex index, string repositoryPath, ID parentId, IEnumerable<ID> templateIds, bool includeSubFolders) : base(index, repositoryPath, parentId, templateIds, includeSubFolders)
+        {
+            this.IncludeSubFolders = includeSubFolders;
+        }
+
+        protected override object Execute(IProviderSearchContext searchContext, Expression expression)
+        {
+            IQueryable<ContactList> queryable = from cl in searchContext.GetQueryable<ContactList>()
+                                                where this.TemplateIds.Contains(cl.TemplateId) && cl.Path.StartsWith(this.RepositoryPath) && cl.IsLatestVersion
+                                                select cl;
+            if (!object.ReferenceEquals(this.ParentId, null) && !this.IncludeSubFolders)
+            {
+                queryable = from contactList in queryable
+                            where contactList.ParentId == this.ParentId
+                            select contactList;
+            }
+            expression = base.BuildContentSearchExpression(expression, null, queryable.Expression);
+            return this.EnsureResultIsNotQueryable(Expression.Lambda(expression, null).Compile().DynamicInvoke(new object[0]));
+        }
+    }
+}
---
+++ src/Sitecore.Support.165176/GetAllContactLists.cs
@@ -0,0 +1,39 @@
+﻿using Sitecore.ContentSearch;
+using Sitecore.Data;
+using Sitecore.ListManagement.Configuration;
+using Sitecore.ListManagement.ContentSearch;
+using Sitecore.ListManagement.ContentSearch.Model;
+using Sitecore.ListManagement.ContentSearch.Pipelines;
+using System.Linq;
+
+
+namespace Sitecore.Support.ListManagement.ContentSearch.Pipelines.GetAllLists
+{
+    public class GetAllContactLists: Sitecore.ListManagement.ContentSearch.Pipelines.ListProcessor
+    {
+        private readonly ISearchIndex index;
+
+        public GetAllContactLists()
+        {
+            this.index = ContentSearchManager.GetIndex(ListManagementSettings.ContactListIndexName);
+        }
+
+        public virtual void Process(ListsArgs args)
+        {
+            Diagnostics.Assert.ArgumentNotNull(args, "args");
+            ID itemId = (!args.RootId.IsNull) ? args.RootId : base.RootId;
+            Sitecore.Data.Items.Item item = base.Database.GetItem(itemId);
+            if (item == null)
+            {
+                args.ResultLists = Enumerable.Empty<ContactList>().AsQueryable<ContactList>();
+                return;
+            }
+            Sitecore.Data.ID[] templateIds = new Sitecore.Data.ID[]
+            {
+        base.TemplateId,
+        base.SegmentedListTemplateId
+            };
+            args.ResultLists = new QueryableProxy<ContactList>(new ContactListQueryProvider(this.index, item.Paths.FullPath, item.ID, templateIds, args.IncludeSubFolders));
+        }
+    }
+}
